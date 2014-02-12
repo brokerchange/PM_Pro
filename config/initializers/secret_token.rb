dd4a0853e5b3c8a5b7b8294dc29c94e2d0ef01f7
Ayman Al-Abdullah <aalab002@gmail.com>
2014-02-12 15:29:52 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PMPro::Application.config.secret_key_base = '6e2b0f1eed7a60e2c660e3f9e02794074b81f8657d2f8b2fe58120db3e496caeafaf2b12a3ced1ca37ba918209ed26536c8f83e16b693aa4f82fcde69b381517'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PMPro::Application.config.session_store :cookie_store, key: '_PM_Pro_session'
